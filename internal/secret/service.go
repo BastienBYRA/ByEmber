000fa160c9ec6783d44cbeae146c299498617170
Bastien <neitsabast@gmail.com>
2025-06-06 16:39:26 +0200

    feat(api): add OpenAPI spec, generate server code, and implement CreateSecret handler

---
+++ internal/secret/service.go
@@ -0,0 +1,16 @@
+package secret
+
+// func CreateSecret(content string, duration int, views int, password string) (openapi.Secret, error) {
+// 	// hash := sha256.Sum256([]byte(password))
+// 	// hashPassword := fmt.Sprintf("%x", hash)
+
+// 	secret := openapi.Secret{
+// 		// ID:             uuid.NewString(),
+// 		// Password:       hashPassword,
+// 		// ExpiresAt:      time.Now().Local().Add(time.Second * time.Duration(duration)),
+// 		// RemainingViews: views,
+// 		// EncryptedText:  "Bob",
+// 	}
+
+// 	return secret, nil
+// }
